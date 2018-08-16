098edfe38448845d3b660a7fb3560147829bd4ff
Maiskar Apparao <apparao@algorythma.com>
2018-08-16 15:01:37 +0530

    [RtmpMarker]: Added changes to properly send the markerinfo type/uid/data/retryindex

--- extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RTMPListener.java
+++ extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RTMPListener.java
@@ -1,5 +1,7 @@
 package com.google.android.exoplayer2.ext.rtmp;
 
+import net.butterflytv.rtmp_client.RTMPMarker;
+
 public interface RTMPListener<S> {
     void onData (S source, RTMPMarker marker);
     void onFunction (S source);
